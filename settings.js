183a2e0fbb665ab0c9e65ee166a626eb5e851aab
pathum177 <pathummalsara177@gmail.com>
2025-08-12 15:13:38 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=IQZ0hQJD#85OPfjGWZs2KqXHUYpCOF3dyPShl6m_Y9wXs7Te9GHc' : process.env.SESSION_ID,
+PREFIX: process.env.PREFIX || '.' ,
+MODE: process.env.MODE === undefined ?"groups" : process.env.MODE,
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS
+};
